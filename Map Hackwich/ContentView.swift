ce91028cbc9aefcf20c9be628c038ee46caa2477
Timothy Price <theCatintheFedora1367@gmail.com>
2021-03-02 10:18:49 -0600

    finished

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(
             latitude: 42.15704,
@@ -17,8 +19,14 @@ struct ContentView: View {
             latitudeDelta: 0.05,
             longitudeDelta: 0.05)
     )
+    
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(
+            coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode
+        )
     }
 }
 
