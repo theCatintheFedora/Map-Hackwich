93def6b4ce4cd22523fc606c8b014b3cc40549e9
Timothy Price <theCatintheFedora1367@gmail.com>
2021-03-03 10:32:35 -0600

    added the geocoded lookup... wow that was annoying : /

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -19,19 +19,65 @@ struct ContentView: View {
             latitudeDelta: 0.05,
             longitudeDelta: 0.05)
     )
+    @State private var places = [Place(name: "Barrington High School", coordinate: CLLocationCoordinate2D(latitude: 42.15704, longitude: -88.14812))]
     
     var body: some View {
         Map(
             coordinateRegion: $region,
             interactionModes: .all,
             showsUserLocation: true,
-            userTrackingMode: $userTrackingMode
-        )
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.coordinate,
+                          anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
+                Marker(name: place.name)
+            }
+        }
+        .onAppear {
+                    findLocation(name: "Springfield")
+        }
+    }
+    func findLocation(name: String) {
+        locationManager.geocoder.geocodeAddressString(name) { (placemarks, error) in
+            if let placemark = placemarks?.first {
+                let place = Place(name: placemark.name!,
+                                  coordinate: placemark.location!.coordinate)
+                places.append(place)
+            }
+            else {
+                print("Could not locate \(name)")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+struct Marker: View {
+    var name: String
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer(minLength: 15)
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .rotationEffect(.degrees(45))
+            }
+            Capsule()
+                .fill(Color.red)
+                .frame(width: 200, height: 30, alignment: .center)
+            Text(name)
+        }
     }
 }
